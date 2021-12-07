7d7b7b693cc50cc67ed09c01c8ae51b48d1821ff
Jonathan Fisher <jonathan@trueanthem.com>
2021-12-06 16:14:00 -0800

    add reddit api subreddit post title and num comments retriever

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Subreaddit() {
+    const [posts, setPosts] = React.useState([]);
+    const [subredditFromNewPost, setSubreddit] = React.useState("");
+    const nameOfSubreddit = 'python';
+    React.useEffect(() => {
+        axios.get("https://www.reddit.com/r/"+nameOfSubreddit +".json")
+            .then(res => {
+                const newPosts = res.data.data.children
+                    .map(obj => obj.data);
+                const subredditFromNewPost = newPosts[0];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+                setPosts(newPosts);
+                setSubreddit(subredditFromNewPost['subreddit_name_prefixed'])
+            });
+    }, []);
+    return (
+
+        <div>
+                <h1>{subredditFromNewPost}</h1>
+
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>{post.title} | <em> {post.num_comments === 1 ? '1 comment' : post.num_comments + ' comments'}</em></li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+ReactDOM.render(<Subreaddit />,  document.getElementById('root'));
